abb291ac452e76d79d1b182c055f912d1bc0d10d
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-05-08 21:04:56 -0500

    router obser

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { title } from 'process';
+import { Router } from 'express';
+import { tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+
+  router = inject(Router)
+
+routes = routes.map((route)=> ({
+  path: route.path,
+  title: `${route.title ?? 'Maps in Angular'}`
+}));
+
+pageTitle$ = this.router.events.pipe(
+  tap((event)=> console.log(event)
+
+));
+
+}
