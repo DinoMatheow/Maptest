5809f2244c26c0054ba26625aa80033fb72dce83
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-05-12 21:23:39 -0500

    map maker and houses point

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './mini-map.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MiniMapComponent { }
--- src/app/pages/makers-page/makers-page.component.ts
+++ src/app/pages/makers-page/makers-page.component.ts
@@ -1,8 +1,18 @@
+import { UUIDTypes } from './../../../../node_modules/uuid/dist/cjs-browser/types.d';
+
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, signal, viewChild } from '@angular/core';
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl, { LngLat, LngLatLike } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { environment } from '../../../environments/environment';
 
+import {v4 as UUIDv4} from 'uuid';
+import { filter } from 'rxjs';
+
 mapboxgl.accessToken = environment.mapboxkey;
+
+interface Marker{
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
 
 @Component({
   selector: 'app-makers-page',
@@ -15,6 +25,7 @@ export class MakersPageComponent implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map');
   map = signal<mapboxgl.Map | null>(null);
+  markers = signal<Marker[]>([])
 
   async ngAfterViewInit(): Promise<void> {
     if(!this.divElement()?.nativeElement) return;
@@ -34,16 +45,16 @@ export class MakersPageComponent implements AfterViewInit {
       zoom: 9, // starting zoom
   });
 
-  const maker = new mapboxgl.Marker({
-    draggable: false,
-    color: 'red',
-  })
-  .setLngLat([-77.07311, -12.02353])
-  .addTo(map);
+//   const maker = new mapboxgl.Marker({
+//     draggable: false,
+//     color: 'red',
+//   })
+//   .setLngLat(event.LngLatBoundsLike)
+//   .addTo(map);
 
-map.on('dragend', (event)=> {
-  console.log(event);
-})
+// map.on('dragend', (event)=> {
+//   console.log(event);
+// })
 
 
 this.mapListeners(map);
@@ -51,8 +62,74 @@ this.mapListeners(map);
 }
 
 mapListeners(map: mapboxgl.Map){
-  console.log('object')
+map.on('click', (event)=> this.mapClick(event));
+
+this.map.set(map);
+
 }
+
+mapClick(event: mapboxgl.MapMouseEvent){
+  if(!this.map()) return ;
+  // console.log(event.lngLat)
+
+  const coords = event.lngLat;
+const map = this.map()!;
+
+const color = '#xxxxxx'.replace(/x/g, (y)=>
+((Math.random() * 16) | 0).toString(16)
+);
+
+  const marker = new mapboxgl.Marker({
+    color: color,
+  })
+  .setLngLat(coords)
+  .addTo(map);
+
+
+const newMarker: Marker = {
+  id: UUIDv4(),
+  mapboxMarker: marker,
+};
+
+this.markers.update((markers) => [newMarker, ...markers]);
+
+console.log(this.markers());
+
+
+
+}
+
+
+
+flyToMarker(lngLat: LngLatLike){
+
+if(!this.map()) return;
+
+this.map()?.flyTo({
+  center: lngLat,
+})
+
+
+}
+
+
+
+deletemarker(marker: Marker){
+if (!this.markers()) return;
+
+const map = this.map()!;
+marker.mapboxMarker.remove();
+
+this.markers.set(this.markers().filter(m=> m.id != marker.id))
+
+}
+
+
+
+
+
+
+
 
 
 
